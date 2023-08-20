5a9fe3bd7d3a100e4c22557b776e073d2fe2fe69
Aditya Narayan Tiwari <98219031+iAdityaNarayanT@users.noreply.github.com>
2023-08-20 10:31:06 +0530

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,74 @@
+import I1 from "../img/f10.png";
+import F1 from "../img/f9.png";
+import C3 from "../img/f8.png";
+import Fi1 from "../img/f7.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Watermelon",
+    decp: "Fresh & Juicy",
+    price: "100",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Banana",
+    decp: "Fresh & Sweet",
+    price: "60",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Cherries",
+    decp: "Fresh & Sweet",
+    price: "120",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Strawberries",
+    decp: "Fresh & Sweet",
+    price: "200",
+    imageSrc: Fi1,
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Vegetable",
+    urlParamName: "vegetable",
+  },
+  {
+    id: 2,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 3,
+    name: "DryFruits",
+    urlParamName: "dryfruits",
+  },
+  {
+    id: 4,
+    name: "Juices",
+    urlParamName: "juices",
+  },
+  {
+    id: 5,
+    name: "Spices",
+    urlParamName: "curry",
+  },
+  {
+    id: 6,
+    name: "Grains",
+    urlParamName: "icecreams",
+  },
+
+  {
+    id: 7,
+    name: "MedicalHerbs",
+    urlParamName: "drinks",
+  },
+];
